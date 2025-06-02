1b60a204247921bfe6d424de2d5d3f0c3afd79f5
saraDoitch <sara0527656182@gmail.com>
2025-06-03 00:39:29 +0300

    add API slices

---
+++ client/src/stores/Slices/endPointsDriver.ts
@@ -0,0 +1,51 @@
+import { Driver, ResDriver } from "../../components/interfaces/Interface";
+import apiSliceDriver from "./apiSliceDrivers";
+
+const apiDriverSlice = apiSliceDriver.injectEndpoints({
+    endpoints: (builder) => ({
+        getAlldrivers: builder.query<Driver[], void>({
+            query: () => "/drivers",
+            providesTags: ["Driver"],
+        }),
+        getdriverById: builder.query<Driver, string>({
+            query: (_id) => `/drivers/${_id}`,
+            providesTags: ["Driver"],
+          }),
+      
+        adddriver: builder.mutation<Driver, Driver>({
+            query: (newdriver) => ({
+                url: "/drivers",
+                             
+                method: "POST",
+                body: newdriver,
+            }),
+            invalidatesTags: ["Driver"],
+        }),
+        updatedriver: builder.mutation<Driver, Driver>({
+            query: (updatedriver) => ({
+                url: `/drivers/${updatedriver._id}`,
+                method: "PUT",
+                body: updatedriver,
+            }),
+            invalidatesTags: ["Driver"],
+        }),
+        deletedriver: builder.mutation<Driver, Driver>({
+            query: (deletedriver) => ({
+                url: `/drivers/${deletedriver._id}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ["Driver"],
+        }),
+        
+    }),
+});
+
+export const {
+    useGetAlldriversQuery,
+    useGetdriverByIdQuery,
+    useAdddriverMutation,
+    useUpdatedriverMutation,
+    useDeletedriverMutation,
+    
+} = apiDriverSlice;
+export default apiDriverSlice
