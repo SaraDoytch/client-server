4bc5d75c0d0a8cc81a13eb06fcef96bf332c610e
saraDoitch <sara0527656182@gmail.com>
2025-06-02 22:37:27 +0300

    adding a router page and store

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,25 @@
 
 
+import { RouterProvider } from 'react-router'
 import './App.css'
-import Home from './components/Home'
 
+// import router from './app/routes/router2'
+import router from "./router/router"
+import { Provider } from 'react-redux'
+import store from './stores/Store'
+import { CookiesProvider } from 'react-cookie'
+// import { Provider } from 'react-redux'
+// import store from './stores/Store'
 function App() {
 
 
   return (
     <>
-<Home></Home>
+    <CookiesProvider>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+</CookiesProvider>
 
     </>
   )
