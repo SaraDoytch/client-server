fba14d38dc1f26fcee5567db093eb9012eaf6ad2
brachaCh <b0556733065@gmail.com>
2025-06-10 21:11:35 +0300

    fix loginIn

---
+++ client/src/stores/Slices/authSlice.ts
@@ -0,0 +1,45 @@
+
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface User {
+  id?: string | number;
+  name?: string;
+  email: string;
+  password: string;
+  phone?: string;
+  gender?: string;
+  hasCar?: boolean;
+  driveringLicense?: string;
+}
+
+interface AuthState {
+  currentUser: User | null;
+}
+
+const initialState: AuthState = {
+  currentUser: localStorage.getItem("currentUser")
+    ? JSON.parse(localStorage.getItem("currentUser") as string)
+    : null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action: PayloadAction<User>) => {
+      state.currentUser = action.payload;
+      localStorage.setItem("currentUser", JSON.stringify(action.payload));
+    },
+      loginRegister: (state, action) => {
+      state.currentUser = action.payload;
+      localStorage.setItem('currentUser', JSON.stringify(action.payload));
+    },
+    logout: (state) => {
+      state.currentUser = null;
+      localStorage.removeItem("currentUser");
+    },
+  },
+});
+
+export const { login, loginRegister, logout } = authSlice.actions;
+export default authSlice.reducer;
