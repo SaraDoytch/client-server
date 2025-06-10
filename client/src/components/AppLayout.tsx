91430d0da4663df88348ae9b23018d0bdebb7079
brachaCh <b0556733065@gmail.com>
2025-06-10 20:37:28 +0300

    fix applayout

--- client/src/components/AppLayout.tsx
+++ client/src/components/AppLayout.tsx
@@ -1,38 +1,15 @@
-// import React from 'react'
 
-// const AppLayout = () => {
-//   return (
-//     <div>AppLayout</div>
-//   )
-// }
-
-// export default AppLayout
-
-
-import { Outlet } from 'react-router';
-import { useEffect, useState } from 'react';
-import UserLayout from './UserLayout';
-import Nav from './Nav';
-import HomePageNav from './HomePageNav';
-import Footer from './Footer'; 
+import UserLayout from './UserLayOut';
+import Footer from './Footer';
 
 const AppLayout = () => {
-  const [baseNav, setBaseNav] = useState<boolean>(true);
-
-  useEffect(() => {
-    const user = localStorage.getItem('currentUser');
-    if (user) {
-      setBaseNav(false);
-    }
-  }, []);
-
   return (
     <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <UserLayout />
-      {/* {baseNav ? <HomePageNav /> : <Nav />} */}
+
       <div style={{ flex: 1 }}>
-        <Outlet />
       </div>
+
       <Footer />
     </div>
   );
