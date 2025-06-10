f0ee09583f37efee05f8d03cf7b3a62939d21fdc
brachaCh <b0556733065@gmail.com>
2025-06-10 14:23:37 +0300

    in server to do routes

--- server/routes/driverSuggestionRoute.js
+++ server/routes/driverSuggestionRoute.js
@@ -1,4 +1,25 @@
 const express = require('express');
 const router = express.Router();
-///
+const {
+    getAllDriverSuggestions,
+    addDriverSuggestion,
+    deleteDriverSuggestion,
+    updateDriverSuggestion,
+    getFoundById,
+    getDriverSuggestions,
+    joinSuggestion,
+    createSuggestion,
+    getPassengerSuggestions
+} = require('../controllers/driverSuggestionController');
+const verifyJWT = require('../middleware/verifyJWT');
+router.get('/', getAllDriverSuggestions);
+router.post('/', addDriverSuggestion)
+router.get('/:id', verifyJWT, getFoundById);
+router.delete('/:id', verifyJWT, deleteDriverSuggestion);
+router.put('/:id', verifyJWT, updateDriverSuggestion);
+router.put('/:suggestionId/joinSuggestion', verifyJWT, joinSuggestion)
+router.post('/createSuggestion', verifyJWT, createSuggestion);
+router.get('/driver/:driverId', verifyJWT, getDriverSuggestions);
+router.get('/passenger/:userId', verifyJWT, getPassengerSuggestions);
+
 module.exports = router;
