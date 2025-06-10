806d8dc6944a7b8306e6bdf3bfdfc64b52254141
brachaCh <b0556733065@gmail.com>
2025-06-10 15:17:43 +0300

    arrange config

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,19 +1,19 @@
 const allowedOrigins = [
-  'http://localhost:5173', 
-  'http://localhost:8000', 
-  'http://localhost:7000', 
+  'http://localhost:5173',
+  'http://localhost:8000',
+  'http://localhost:7000',
   'http://localhost:5000',
-  'http://localhost:3000' 
+  'http://localhost:3000'
 ];
 const corsOptions = {
   origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) { 
-          callback(null, true);
-      } else {
-          callback(new Error('Not allowed by CORS'));
-      }
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
   },
-  credentials: true, 
+  credentials: true,
 
   optionsSuccessStatus: 200
 };
